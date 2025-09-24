d17b250888829d5219db70957c4af36127151e23
Rachel Mejia <rachel.mejia@utp.ac.pa>
2025-09-23 19:03:51 -0500

    Laboratorio51 completado

---
+++ Laboratorio5/Laboratorio5/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+
+    private int[] sueldos; //Declaramos un vector
+
+    public void Cargar()
+    {
+        sueldos = new int[6];// Inicializamos el vector en 5
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operario " + f + ":);
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);//Asignamos los 5 sueldos al vector
+        }
+    }
+    //Muestra los sueldos de los operarios en el vector sueldos[f]
+    public void Imprimir()
+    {
+        Console.Write("los 5 sueldos de los aperarios \n");
+        for(int f=1; f <= 5; f++)
+        {
+            Console.Write("[" + sueldos[f] + "]");
+        }
+        Console.ReadKey();
+    }
+    //main principal
+    static void main(string[] args)
+    {
+        PruebaVector1 pv = new PruebaVector1();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+
+
+        }
+
+    }
+}
+
