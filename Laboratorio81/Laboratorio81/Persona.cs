a93e78714bd81b5f1e692acb197c371456796c5f
Rachel Mejia <rachel.mejia@utp.ac.pa>
2025-09-30 19:04:10 -0500

    lab81 completo

---
+++ Laboratorio81/Laboratorio81/Persona.cs
@@ -0,0 +1,26 @@
+﻿
+    class Persona
+    {
+        //Campo de cada objeto Persona que almacena su nombre
+
+        public string Nombre;
+        //Campo de cada objeto Persona que almacena su edad
+        public int Edad;
+        //Campo de cada objeto Persona que almacena su NIF
+
+        public string NIF;
+
+        void Cumpleaños() // Incrementa en uno de edad del objeto Persona
+        {
+            Edad++;
+        }
+        //Constructor de Persona
+        public Persona(string nombre, int edad, string nif)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            NIF = nif;
+        }
+    }
+
+
