e9d284290145c531b383dae7f4410d204227b662
Rachel Mejia <rachel.mejia@utp.ac.pa>
2025-09-30 20:14:47 -0500

    Laboratorio88

---
+++ Laboratorio 8/Laboratorio88/Laboratorio88/ClaseAbstracta.cs
@@ -0,0 +1,15 @@
+﻿abstract class  ClaseAbstracta
+{
+    //Se fuerza la herencia de la clase para definir estos metodos
+    abstract protected string tomarValor();
+    abstract public string prefixValor(string prefix);
+    //Metodo comun
+    public void printOut()
+    {
+        Console.WriteLine(tomarValor());
+    }
+}
+
+
+
+
