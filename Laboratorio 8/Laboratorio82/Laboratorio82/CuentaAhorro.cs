28e5962a09e180958cb17bd22e3366c4faa6c4e9
Rachel Mejia <rachel.mejia@utp.ac.pa>
2025-09-30 19:13:39 -0500

    Laboratorio82 completo

---
+++ Laboratorio 8/Laboratorio82/Laboratorio82/CuentaAhorro.cs
@@ -0,0 +1,14 @@
+﻿public class CuentaAhorro : Cuenta
+{
+    public CuentaAhorro(string prmttIdCuenta) : base(prmttIdCuenta)
+    {
+    }
+
+    public override void CalcularIntereses()
+    {
+        System.Console.WriteLine(
+            "CuentaAhorro.CalcularIntereses() efectuado para " +
+            "la cuenta {0}", getIdCuenta());
+    }
+
+}
