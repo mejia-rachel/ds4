f8c610095001d08f2d8c120cc7d7c4bb6af3b043
Rachel Mejia <rachel.mejia@utp.ac.pa>
2025-10-14 18:47:32 -0500

    sintaxis lab 11 completo

---
+++ Parcial1/Parcial1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int Numerito;
+
+        do
+        {
+            Console.Write("Ingrese un numero impar: ");
+            Numerito = int.Parse(Console.ReadLine());
+        } while (Numerito % 2 == 0);
+
+        int[,] matriz = new int[Numerito, Numerito];
+        int suma = 0;
+        int centro = Numerito / 2;
+
+        for (int i = 0; i < Numerito; i++)
+        {
+            for (int j = 0; j < Numerito; j++)
+            {
+                if (i == 0 || i == Numerito - 1 || j == centro)
+                {
+                    matriz[i, j] = rnd.Next(101, 201);
+                    suma += matriz[i, j];
+                }
+                else
+                {
+                    matriz[i, j] = 0;
+                }
+                Console.Write($"{matriz[i, j],5}");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("La suma de todos los elementos aleatorios es: " + suma);
+    }
+}
